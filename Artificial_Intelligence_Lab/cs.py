8e10f123dd4e1a2b0cb2a33dc9d07d3ecd38c3ab
Mayuresh Pathak <mayureshpathak001@gmail.com>
2025-07-17 12:03:58 +0530

    file cs.py is updated

--- Artificial_Intelligence_Lab/cs.py
+++ Artificial_Intelligence_Lab/cs.py
@@ -1,16 +1,16 @@
 print("Select any two prefered subjects : \n1.Maths\n2.Physics\n3.Biology\n4.Programming\n5.Circuits\n6.Electronics\n7.Statistics\n8.AI Concepts\n9.Chemistry\n")
-sub1=int(input("Enter your First Prefered Subject Number : "))
-sub2=int(input("Enter your Second Prefered Subject Number : "))
-if (sub1==1 and sub2==2) or (sub1==2 and sub2==1) :
+sub1=str(input("Enter your First Prefered Subject : "))
+sub2=str(input("Enter your Second Prefered Subject : "))
+if ((sub1=="maths" and sub2=="physics") or (sub1=="physics" and sub2=="maths")) :
 	print("\nCareer Suggested - Mechanical Engineering\n")
-elif (sub1==1 and sub2==4) or (sub1==4 and sub2==1)  :
+elif ((sub1=="maths" and sub2=="programming") or (sub1=="programming" and sub2=="maths") ) :
 	print("\nCareer Suggested - Computer Engineering\n")
-elif (sub1==3 and sub2==9) or (sub1==9 and sub2==3) :
+elif ((sub1=="biology" and sub2=="chemistry") or (sub1=="chemistry" and sub2=="biology")) :
 	print("\nCareer Suggested - Biotechnology \n")
-elif (sub1==1 and sub2==5) or (sub1==5 and sub2==1) :
-	print("\nCareer Suggested - Electronics Engineering\n")
-elif (sub1==4 and sub2==7) or (sub1==7 and sub2==4) :
+elif (sub1=="maths" and sub2=="circuits") or (sub1=="circuits" and sub2=="maths") :
+	print("\nCareer Suggested - electronics Engineering\n")
+elif (sub1=="programming" and sub2=="statistics") or (sub1=="statistics" and sub2=="programming") :
 	print("\nCareer Suggested - Artificial Intelligence and Data Science\n")
-elif (sub1==4 and sub2==8) or (sub1==8 and sub2==4) :
+elif (sub1=="programming" and sub2=="ai concepts") or (sub1=="ai concepts" and sub2=="programming") :
 	print("\nCareer Suggested - Artificial Intelligence and Machine Learning Engineering\n")
 
