965c1e3865bd1d01b0316fcd505e232b77750c7e
MayureshPathak <mayureshpathak001@gmail.com>
2025-07-17 19:43:41 +0530

    cs.py

--- Artificial_Intelligence_Lab/cs.py
+++ Artificial_Intelligence_Lab/cs.py
@@ -1,6 +1,6 @@
 print("Select any two prefered subjects : \n1.Maths\n2.Physics\n3.Biology\n4.Programming\n5.Circuits\n6.Electronics\n7.Statistics\n8.AI Concepts\n9.Chemistry\n")
-sub1=str(input("Enter your First Prefered Subject : "))
-sub2=str(input("Enter your Second Prefered Subject : "))
+sub1=str(input("Enter your First Prefered Subject : ")).lower()
+sub2=str(input("Enter your Second Prefered Subject : ")).lower()
 if ((sub1=="maths" and sub2=="physics") or (sub1=="physics" and sub2=="maths")) :
 	print("\nCareer Suggested - Mechanical Engineering\n")
 elif ((sub1=="maths" and sub2=="programming") or (sub1=="programming" and sub2=="maths") ) :
